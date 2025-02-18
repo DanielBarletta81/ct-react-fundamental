261f7518ae91e83cdd5d8aa00d0921c1794ff117
DanielBarletta81 <dbarcelona1481@gmail.com>
2025-02-18 18:45:56 -0500

    adding more files

--- src/About.jsx
+++ src/About.jsx
@@ -5,23 +5,34 @@ const About = () => {
     <section>
       <h2>About Tony Stark</h2>
       <p>
-      Tony Stark, also known as Iron Man, is a Marvel Comics character who is a billionaire, inventor, and superhero. 
+      Tony Stark, also known as Iron Man, is a Marvel Comics character who
+       is a billionaire, inventor, and superhero. 
+       <p>
 Early life:
 Born to Howard and Maria Stark, the owners of Stark Industries 
 Built his first circuit board at age 4 and his first engine at age 7 
 Graduated from MIT at age 17 
-Career:
+</p>
+
+
+<p>Career:
 Worked for Stark Industries, but preferred a playboy lifestyle 
 Inherited Stark Industries at age 21 after his parents died in a car crash 
 Became famous for his professional life and infamous for his personal life 
+</p>
+
+<p>Personal life:
 Iron Man:
 Developed the first Iron Man suit in a cave in Afghanistan 
 Used the suit to escape captors and fight alongside the Avengers 
 Realized that helping people was more thrilling than making money 
-Other traits: 
+</p>
+
+<p>Other traits: 
 Known for his confidence and self-assurance
 Believes in himself and his abilities
 Uses his genius to create high-tech solutions to problems
+</p>
       </p>
     </section>
   );
